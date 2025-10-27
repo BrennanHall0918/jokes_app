5b9845225585bcb4efd01ada9f5d2d64e03c8adb
BrennanHall <mercuryfree247@gmail.com>
2025-10-27 13:59:11 -0500

    added home and error pages

--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-// http://localhost:3000
+// http://localhost:3000 => home page
 router.get('/', (req, res)=> {
-    res.send('working...')
+    // res.send('working...') => display home page
+    res.render('pages/home', {
+        title: "Brennan's Joke App",
+        name: "brennan's joke's app!"
+    })
 })
 
 router.use('/jokes', require('./api/jokesRoutes'))
 
 // error handling
 router.use((req, res, next)=> {
-    res.status(404).send("404 Error. This page does not exist.")
+    res.status(404)
+    .render('pages/404', {
+        title: '404 Error',
+        name: '404 Error. This page does not exist...'
+    })
 })
 
 module.exports = router
